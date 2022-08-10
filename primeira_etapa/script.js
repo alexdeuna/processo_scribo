3ae4202fdfbddc80ed3b4806f8d1c40c4ceab8fe
AlexDeUna <alexdeuna@gmail.com>
2022-08-09 21:20:24 -0300

    primeiro desafio V.1

---
+++ primeira_etapa/script.js
@@ -0,0 +1,17 @@
+function sum() {
+  const number = parseInt(document.getElementById("number").value);
+
+  if (Number.isInteger(number) && number > 0) {
+    let i = 1;
+    let s = 0;
+    for (; i < number;) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        s += i;
+      }
+      ++i;
+    }
+    alert(s);
+  } else {
+    alert("Favor digitar um número inteiro e positivo!");
+  }
+}
